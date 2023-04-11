8d0d4d397f9c95eb5f2cbeb073f243425b12b72e
Martti Louhivuori <martti.louhivuori@csc.fi>
2023-04-11 17:06:59 +0300

    Add support for HOP_SOURCE_*

--- hop/hop_runtime.h
+++ hop/hop_runtime.h
@@ -1,3 +1,11 @@
+#if defined(HOP_SOURCE_HIP)
+#include "source/hip/hip/hip_runtime.h"
+
+#elif defined(HOP_SOURCE_CUDA)
+#include "source/cuda/cuda_runtime.h"
+
+#endif
+
 #if defined(HOP_TARGET_HIP)
 #include "hop_runtime_hip.h"
 
