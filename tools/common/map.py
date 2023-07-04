aaccc0bac2d3c0c5d9bb99d756cb24a97cee1080
Martti Louhivuori <martti.louhivuori@csc.fi>
2023-07-04 16:00:33 +0300

    Translator for identifiers / filenames

--- tools/common/map.py
+++ tools/common/map.py
@@ -43,3 +43,53 @@ class Map(collections.UserDict):
         raise ValueError(
                 "Unable to guess translation for identifier "
                 "'{}' in {} {} map".format(key, self.label.upper(), kind))
+
+
+class Translator:
+    regex_lower = re.compile('^(.*?)(gpu|hip|cuda|cu)')
+    regex_camel = re.compile('^(.*?)(Gpu|Hip|Cuda|Cu)')
+    regex_upper = re.compile('^(.*?)(GPU|HIP|CUDA|CU)')
+    regex_default_lower = re.compile('^()(gpu|hip|cuda)')
+    regex_default_camel = re.compile('^()(Gpu|Hip|Cuda)')
+    regex_default_upper = re.compile('^()(GPU|HIP|CUDA)')
+
+    def _translate(self, name, target, default=False):
+        if default:
+            _regex_lower = self.regex_default_lower
+            _regex_camel = self.regex_default_camel
+            _regex_upper = self.regex_default_upper
+        else:
+            _regex_lower = self.regex_lower
+            _regex_camel = self.regex_camel
+            _regex_upper = self.regex_upper
+        if _regex_lower.match(name):
+            return _regex_lower.sub(r'\1' + target, name)
+        if _regex_camel.match(name):
+            return _regex_camel.sub(r'\1' + target.capitalize(), name)
+        if _regex_upper.match(name):
+            return _regex_upper.sub(r'\1' + target.upper(), name)
+        return name
+
+    def translate(self, name, target):
+        return self._translate(name, target)
+
+    def to_hop(self, name, default=False):
+        return self.translate(name, 'gpu')
+
+    def to_hip(self, name, default=False):
+        return self.translate(name, 'hip')
+
+    def to_cuda(self, name, default=False):
+        return self.translate(name, 'cuda')
+
+    def default(self, name, target):
+        return self._translate(name, target, True)
+
+    def is_default_hip(self, hop, hip):
+        return hip == self.default(hop, 'hip')
+
+    def is_default_cuda(self, hop, cuda):
+        return cuda == self.default(hop, 'cuda')
+
+
+translate = Translator()
