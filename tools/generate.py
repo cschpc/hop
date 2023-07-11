1f19a07c3f23cbfd545c750026c1feff7d33cb50
Martti Louhivuori <martti.louhivuori@csc.fi>
2023-07-11 16:01:26 +0300

    Add debug logging

--- tools/generate.py
+++ tools/generate.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import logging
+
 from common.headers import make_headers
 from common.io import read_tree, read_map, read_list, write_header
 from common.parser import ArgumentParser
@@ -16,11 +18,16 @@ def generate(args):
             'hip': read_list('data/hip.list'),
             'cuda': read_list('data/cuda.list'),
             }
+    logging.debug('tree={}'.format(tree))
+    logging.debug('id_maps={}'.format(id_maps))
+    logging.debug('id_lists={}'.format(id_lists))
 
     headers = make_headers(tree, id_maps, id_lists)
     for path, content in headers.items():
         if args.verbose:
             print('Writing header: {}'.format(path))
+        logging.debug('path={}'.format(path))
+        logging.debug('content={}'.format(content))
         if not args.dry_run:
             write_header(path, content, args.force)
 
@@ -35,7 +42,16 @@ if __name__ == '__main__':
             help='force overwriting of existing files')
     parser.add_argument('-v', '--verbose', action='store_true', default=False,
             help='display additional information while running')
+    parser.add_argument('--debug', action='store_true', default=False,
+            help='display additional information while running')
 
     args = parser.parse_args()
+
+    # configure logging
+    config = {'format': '[%(levelname)s] %(message)s'}
+    if args.debug:
+        config['level'] = logging.DEBUG
+    logging.basicConfig(**config)
+    logging.debug('args={}'.format(args))
 
     generate(args)
