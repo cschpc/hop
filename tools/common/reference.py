63461a1775600af30d251ca1b696c151bb5b3431
Martti Louhivuori <martti.louhivuori@csc.fi>
2023-09-03 13:41:28 +0300

    Reference ID mapping

---
+++ tools/common/reference.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import logging
+
+from common.scrape import scrape_hipify
+
+
+def reference_map(path):
+    triplets = scrape_hipify(path)
+    reference = {
+            'hip': {},
+            'cuda': {},
+            }
+    for hop, hip, cuda in triplets:
+        reference['cuda'][cuda] = hip
+        reference['hip'].setdefault(hip, [])
+        reference['hip'][hip].append(cuda)
+    logging.debug('reference={}'.format(reference))
+    return reference
