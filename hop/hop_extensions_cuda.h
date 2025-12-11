e327a30f9e567e344fca919acb90cce8935c919d
Martti Louhivuori <martti.louhivuori@csc.fi>
2025-12-11 13:51:04 +0200

    Remove const from arguments

--- hop/hop_extensions_cuda.h
+++ hop/hop_extensions_cuda.h
@@ -5,47 +5,47 @@
  *   data types: cuDoubleComplex, cuFloatComplex, <T> for scalars
  */
 __host__ __device__ __inline__
-cuDoubleComplex operator+(cuDoubleComplex const &a, cuDoubleComplex const &b)
+cuDoubleComplex operator+(cuDoubleComplex &a, cuDoubleComplex &b)
 {
     return cuCadd(a, b);
 }
 __host__ __device__ __inline__
-cuFloatComplex operator+(cuFloatComplex const &a, cuFloatComplex const &b)
+cuFloatComplex operator+(cuFloatComplex &a, cuFloatComplex &b)
 {
     return cuCaddf(a, b);
 }
 __host__ __device__ __inline__
-cuDoubleComplex operator+(cuFloatComplex const &a, cuDoubleComplex const &b)
+cuDoubleComplex operator+(cuFloatComplex &a, cuDoubleComplex &b)
 {
     return cuCadd(cuComplexFloatToDouble(a), b);
 }
 __host__ __device__ __inline__
-cuDoubleComplex operator+(cuDoubleComplex const &a, cuFloatComplex const &b)
+cuDoubleComplex operator+(cuDoubleComplex &a, cuFloatComplex &b)
 {
     return cuCadd(a, cuComplexFloatToDouble(b));
 }
 
 template <typename T>
 __host__ __device__ __inline__
-cuDoubleComplex operator+(cuDoubleComplex const &a, T const &b)
+cuDoubleComplex operator+(cuDoubleComplex &a, T &b)
 {
     return cuCadd(a, make_cuDoubleComplex(b, 0));
 }
 template <typename T>
 __host__ __device__ __inline__
-cuDoubleComplex operator+(T const &a, cuDoubleComplex const &b)
+cuDoubleComplex operator+(T &a, cuDoubleComplex &b)
 {
     return cuCadd(make_cuDoubleComplex(a, 0), b);
 }
 template <typename T>
 __host__ __device__ __inline__
-cuFloatComplex operator+(cuFloatComplex const &a, T const &b)
+cuFloatComplex operator+(cuFloatComplex &a, T &b)
 {
     return cuCaddf(a, make_cuFloatComplex(b));
 }
 template <typename T>
 __host__ __device__ __inline__
-cuFloatComplex operator+(T const &a, cuFloatComplex const &b)
+cuFloatComplex operator+(T &a, cuFloatComplex &b)
 {
     return cuCaddf(make_cuFloatComplex(a), b);
 }
@@ -57,47 +57,47 @@ cuFloatComplex operator+(T const &a, cuFloatComplex const &b)
  *   data types: cuDoubleComplex, cuFloatComplex, <T> for scalars
  */
 __host__ __device__ __inline__
-cuDoubleComplex operator-(cuDoubleComplex const &a, cuDoubleComplex const &b)
+cuDoubleComplex operator-(cuDoubleComplex &a, cuDoubleComplex &b)
 {
     return cuCsub(a, b);
 }
 __host__ __device__ __inline__
-cuFloatComplex operator-(cuFloatComplex const &a, cuFloatComplex const &b)
+cuFloatComplex operator-(cuFloatComplex &a, cuFloatComplex &b)
 {
     return cuCsubf(a, b);
 }
 __host__ __device__ __inline__
-cuDoubleComplex operator-(cuFloatComplex const &a, cuDoubleComplex const &b)
+cuDoubleComplex operator-(cuFloatComplex &a, cuDoubleComplex &b)
 {
     return cuCsub(cuComplexFloatToDouble(a), b);
 }
 __host__ __device__ __inline__
-cuDoubleComplex operator-(cuDoubleComplex const &a, cuFloatComplex const &b)
+cuDoubleComplex operator-(cuDoubleComplex &a, cuFloatComplex &b)
 {
     return cuCsub(a, cuComplexFloatToDouble(b));
 }
 
 template <typename T>
 __host__ __device__ __inline__
-cuDoubleComplex operator-(cuDoubleComplex const &a, T const &b)
+cuDoubleComplex operator-(cuDoubleComplex &a, T &b)
 {
     return cuCsub(a, make_cuDoubleComplex(b, 0));
 }
 template <typename T>
 __host__ __device__ __inline__
-cuDoubleComplex operator-(T const &a, cuDoubleComplex const &b)
+cuDoubleComplex operator-(T &a, cuDoubleComplex &b)
 {
     return cuCsub(make_cuDoubleComplex(a, 0), b);
 }
 template <typename T>
 __host__ __device__ __inline__
-cuFloatComplex operator-(cuFloatComplex const &a, T const &b)
+cuFloatComplex operator-(cuFloatComplex &a, T &b)
 {
     return cuCsubf(a, make_cuFloatComplex(b, 0));
 }
 template <typename T>
 __host__ __device__ __inline__
-cuFloatComplex operator-(T const &a, cuFloatComplex const &b)
+cuFloatComplex operator-(T &a, cuFloatComplex &b)
 {
     return cuCsubf(make_cuFloatComplex(a, 0), b);
 }
@@ -109,47 +109,47 @@ cuFloatComplex operator-(T const &a, cuFloatComplex const &b)
  *   data types: cuDoubleComplex, cuFloatComplex, <T> for scalars
  */
 __host__ __device__ __inline__
-cuDoubleComplex operator*(cuDoubleComplex const &a, cuDoubleComplex const &b)
+cuDoubleComplex operator*(cuDoubleComplex &a, cuDoubleComplex &b)
 {
     return cuCmul(a, b);
 }
 __host__ __device__ __inline__
-cuFloatComplex operator*(cuFloatComplex const &a, cuFloatComplex const &b)
+cuFloatComplex operator*(cuFloatComplex &a, cuFloatComplex &b)
 {
     return cuCmulf(a, b);
 }
 __host__ __device__ __inline__
-cuDoubleComplex operator*(cuFloatComplex const &a, cuDoubleComplex const &b)
+cuDoubleComplex operator*(cuFloatComplex &a, cuDoubleComplex &b)
 {
     return cuCmul(cuComplexFloatToDouble(a), b);
 }
 __host__ __device__ __inline__
-cuDoubleComplex operator*(cuDoubleComplex const &a, cuFloatComplex const &b)
+cuDoubleComplex operator*(cuDoubleComplex &a, cuFloatComplex &b)
 {
     return cuCmul(a, cuComplexFloatToDouble(b));
 }
 
 template <typename T>
 __host__ __device__ __inline__
-cuDoubleComplex operator*(cuDoubleComplex const &a, T const &b)
+cuDoubleComplex operator*(cuDoubleComplex &a, T &b)
 {
     return cuCmul(a, make_cuDoubleComplex(b, 0));
 }
 template <typename T>
 __host__ __device__ __inline__
-cuDoubleComplex operator*(T const &a, cuDoubleComplex const &b)
+cuDoubleComplex operator*(T &a, cuDoubleComplex &b)
 {
     return cuCmul(make_cuDoubleComplex(a, 0), b);
 }
 template <typename T>
 __host__ __device__ __inline__
-cuFloatComplex operator*(cuFloatComplex const &a, T const &b)
+cuFloatComplex operator*(cuFloatComplex &a, T &b)
 {
     return cuCmul(a, make_cuFloatComplex(b, 0));
 }
 template <typename T>
 __host__ __device__ __inline__
-cuFloatComplex operator*(T const &a, cuFloatComplex const &b)
+cuFloatComplex operator*(T &a, cuFloatComplex &b)
 {
     return cuCmul(make_cuFloatComplex(a, 0), b);
 }
@@ -161,35 +161,35 @@ cuFloatComplex operator*(T const &a, cuFloatComplex const &b)
  *   data types: cuDoubleComplex, cuFloatComplex, <T> for scalars
  */
 __host__ __device__ __inline__
-void operator+=(cuDoubleComplex &a, cuDoubleComplex const &b)
+void operator+=(cuDoubleComplex &a, cuDoubleComplex &b)
 {
     a = cuCadd(a, b);
 }
 __host__ __device__ __inline__
-void operator+=(cuFloatComplex &a, cuFloatComplex const &b)
+void operator+=(cuFloatComplex &a, cuFloatComplex &b)
 {
     a = cuCaddf(a, b);
 }
 __host__ __device__ __inline__
-void operator+=(cuFloatComplex &a, cuDoubleComplex const &b)
+void operator+=(cuFloatComplex &a, cuDoubleComplex &b)
 {
     a = cuCaddf(a, cuComplexDoubleToFloat(b));
 }
 __host__ __device__ __inline__
-void operator+=(cuDoubleComplex &a, cuFloatComplex const &b)
+void operator+=(cuDoubleComplex &a, cuFloatComplex &b)
 {
     a = cuCadd(a, cuComplexFloatToDouble(b));
 }
 
 template <typename T>
 __host__ __device__ __inline__
-void operator+=(cuDoubleComplex &a, T const &b)
+void operator+=(cuDoubleComplex &a, T &b)
 {
     a = cuCadd(a, make_cuDoubleComplex(b));
 }
 template <typename T>
 __host__ __device__ __inline__
-void operator+=(cuFloatComplex &a, T const &b)
+void operator+=(cuFloatComplex &a, T &b)
 {
     a = cuCadd(a, make_cuFloatComplex(b));
 }
